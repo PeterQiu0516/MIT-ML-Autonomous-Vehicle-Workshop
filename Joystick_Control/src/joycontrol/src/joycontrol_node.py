e204c253db38cfb2086d94c70b810acd32be3e48
PeterQiu0516 <peterqiu0516@gmail.com>
2021-01-05 08:26:36 +0800

    2021/01/05 Clean-up

--- Joystick_Control/src/joycontrol/src/joycontrol_node.py
+++ Joystick_Control/src/joycontrol/src/joycontrol_node.py
@@ -4,25 +4,26 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
+
 class JoyNode(object):
     def __init__(self):
         rospy.init_node('robocar_joy_teleop')
 
         rospy.Subscriber("joy", Joy, self.callback)
-        self.pub = rospy.Publisher('cmd_vel',Twist,queue_size=1)
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
         self.throttle = 0.0
         self.steer = 0.0
         self.twist = Twist()
 
-        self.max_linear_vel = rospy.get_param('~max_linear_vel',0.2)
-        self.max_angular_vel = rospy.get_param('~max_angular_vel',1.5707)
+        self.max_linear_vel = rospy.get_param('~max_linear_vel', 0.2)
+        self.max_angular_vel = rospy.get_param('~max_angular_vel', 1.5707)
 
         self.loop()
 
     def callback(self, data):
-      self.throttle = 2*(-data.axes[5]+data.axes[2])
-      self.steer = -data.axes[0]
+        self.throttle = 2*(-data.axes[5]+data.axes[2])
+        self.steer = -data.axes[0]
         # right trigger (axes 5) = accelerate
         # left trigger (axes 2) = reverse / brake
         # left joystick (axes 0) = steer
@@ -30,12 +31,11 @@ class JoyNode(object):
         #######################################################
         # TO-DO:
         # Assign values to self.throttle and self.steer
-        # notes: 
+        # notes:
         #    - access values by the axes, e.g. data.axes[5]
         #    - keep self.throttle values range from -1 to 1
         #    - keep self.steer value range from -1 to 1
         #######################################################
-
 
     def loop(self):
 
@@ -43,11 +43,12 @@ class JoyNode(object):
         rate = rospy.Rate(1/dt)
 
         while not rospy.is_shutdown():
-            #print 'throttle: ' + str(self.throttle) + ', steer: ' + str(self.steer)
+            # print 'throttle: ' + str(self.throttle) + ', steer: ' + str(self.steer)
             self.twist.linear.x = self.max_linear_vel * self.throttle
             self.twist.angular.z = self.max_angular_vel * self.steer
             self.pub.publish(self.twist)
             rate.sleep()
+
 
 if __name__ == '__main__':
     JoyNode()
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MIT-ML-Autonomous-Vehicle-Workshop
 A research-oriented educational workshop entitled "Machine Learning and Autonomous Vehicle" held on campus of MIT during 2019 Winter.
 
-Professor: Tze-Ping Low, Li Ding and Tom Bertalan
+Instructor: Prof. Tom Bertalan, Tze-Ping Low and Li Ding
 
 During the workshop, I
 + Assembled, programed and tested small scale self-driving vehicles based on Raspberry Pi, ROS and Python.
@@ -9,7 +9,16 @@ During the workshop, I
 + Built SVM and CNN classifiers by Scikit-learn and TensorFlow to accomplish traffic sign recognition
 + Worked as the team leader and win the 2nd prize on the match day
 
-Main coding scripts could be found in `MIT-Race-Car/main/src/`.
+## main
 
+Main coding scripts could be found in `main/src/`.
+
+## Joystick_Control
+
+Scripts for enabling controlling the robocar using a Joystick. 
+
+## Slides
+
+Collection of slides.
 
 
